6cc61559a903169404286c5b5787ed2f0b34dcdd
Thiago Cesar Andrade <90931093+AndradeTC86@users.noreply.github.com>
2024-11-21 12:17:40 +0000

    Finalizando exercício

--- python_code/calcular_media.py
+++ python_code/calcular_media.py
@@ -1,1 +1,19 @@
 # Descrição: Agora vamos calcular a média de três notas fornecidas na entrada do usuário. 
+
+def calcular_media():
+    try:
+        nota1 = int(input("Digite a primeira nota: "))
+        nota2 = int(input("Digite a segunda nota: "))
+        nota3 = int(input("Digite a terceira nota: "))
+        
+        media = (nota1 + nota2 + nota3) / 3
+        
+        print(f"A média das notas é: {media:.2f}")
+        
+    except ValueError:
+        print("Erro: Certifique-se de que os números informados são válidos.")
+        
+
+if __name__ == "__main__":
+    calcular_media()
+
--- python_code/verificar_palindromo.py
+++ python_code/verificar_palindromo.py
@@ -1,1 +1,17 @@
 # Descrição: Vamos testar se uma palavra é um palíndromo?!
+
+def verificar_palindromo():
+    palavra = input("Digite uma palavra: ")
+    
+    palavra = palavra.replace("", "").lower()
+    
+    palavra_invertida = palavra[::-1]
+    
+    if palavra == palavra_invertida:
+        print(f"A palavra {palavra} é um palíndromo!")
+    else:
+        print(f"A palavra {palavra} não é um palíndromo!")
+        
+    
+if __name__ == "__main__":
+    verificar_palindromo()
