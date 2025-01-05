ce601d1d99ac1705839a8d8806409822e5dc4376
AndradeTC86 <andrade-tc@uol.com.br>
2025-01-05 07:09:26 -0300

    Calcular media

--- src/calcular_media.py
+++ src/calcular_media.py
@@ -5,10 +5,11 @@ class CalculadoraMedia:
         self.notas = []
 
     def adicionar_nota(self, nota):
-        if isinstance(nota, int):
+        try:
+            nota = float(nota)
             self.notas.append(nota)
-        else:
-            raise ValueError("A nota deve ser um número inteiro.")
+        except ValueError:
+            raise ValueError("A nota deve ser um número.")
 
     def calcular_media(self):
         if len(self.notas) != 3:
@@ -19,10 +20,9 @@ if __name__ == "__main__":
     calculadora = CalculadoraMedia()
     try:
         for i in range(1, 4):
-            nota = int(input(f"Digite a {i+1}ª nota: "))
+            nota = input(f"Digite a {i}ª nota: ")
             calculadora.adicionar_nota(nota)
         media = calculadora.calcular_media()
         print(f"A média das notas é: {media:.2f}")
     except ValueError as e:
         print(f"Erro: {e}")
-
--- tests/test_calculadora_media.py
+++ tests/test_calculadora_media.py
@@ -10,12 +10,27 @@ def test_calcular_media():
 
 def test_adicionar_nota_invalida():
     calculadora = CalculadoraMedia()
-    with pytest.raises(ValueError):
-        calculadora.adicionar_nota("nota invalida")
+    with pytest.raises(ValueError, match="A nota deve ser um número."):
+        calculadora.adicionar_nota("A")
+
 
 def test_calcular_media_notas_insuficientes():
     calculadora = CalculadoraMedia()
     calculadora.adicionar_nota(7)
     calculadora.adicionar_nota(8)
-    with pytest.raises(ValueError):
+    with pytest.raises(ValueError, match="Devem ser fornecidas três notas para calcular a média."):
+        calculadora.calcular_media()
+
+def test_calcular_media_sem_notas():
+    calculadora = CalculadoraMedia()
+    with pytest.raises(ValueError, match="Devem ser fornecidas três notas para calcular a média."):
+        calculadora.calcular_media()
+
+def test_calcular_media_mais_de_tres_notas():
+    calculadora = CalculadoraMedia()
+    calculadora.adicionar_nota(6)
+    calculadora.adicionar_nota(7)
+    calculadora.adicionar_nota(8)
+    calculadora.adicionar_nota(9)
+    with pytest.raises(ValueError, match="Devem ser fornecidas três notas para calcular a média."):
         calculadora.calcular_media()
